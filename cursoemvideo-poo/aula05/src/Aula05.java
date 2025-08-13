a5fe362231190437000f9943a3c3f8cdcec4cec3
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-08-13 13:53:33 -0300

    refactor: reorganiza solução do exercicio

--- cursoemvideo-poo/aula05/src/Aula05.java
+++ cursoemvideo-poo/aula05/src/Aula05.java
@@ -2,8 +2,16 @@ public class Aula05 {
     public static void main(String[] args) {
         ContaBanco c1 = new ContaBanco();
         c1.abrirConta("CC","Gabriel Henrique");
+        c1.setNumConta(21312);
         c1.depositar(1500f);
+        c1.sacar(200f);
+        System.out.println(c1.getSaldo());
+        c1.pagarMensal("CC");
+        System.out.println(c1.getSaldo());
+        c1.fecharConta();
+        c1.sacar(21312312312312f);
 
+        c1.estadoAtual();
     }
 
 }
--- cursoemvideo-poo/aula05/src/ContaBanco.java
+++ cursoemvideo-poo/aula05/src/ContaBanco.java
@@ -10,47 +10,71 @@ public class ContaBanco {
     public ContaBanco() {
         this.status = false;
         this.saldo = 0f;
-        this.numConta = 0;
+    }
+
+    public void estadoAtual() {
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.getStatus());
     }
 
     public void abrirConta(String tipo, String nome) {
         this.dono = nome;
-        this.numConta += 1;
         this.status = true;
         if (Objects.equals(tipo, "CC")) {
-            this.saldo += 50;
+            this.saldo = 50;
             this.tipo = tipo;
         }
         else if (Objects.equals(tipo, "CP")) {
-            this.saldo += 150;
+            this.saldo = 150;
             this.tipo = tipo;
         } else {
             System.out.println("ERRO: Tipo de conta invalido.");
+            return;
+        }
+        System.out.println("Conta aberta com sucesso!");
+    }
+
+    public void fecharConta() {
+        if (this.saldo == 0 && this.status) {
+            this.status = false;
+        }
+        else {
+            System.out.println("ERRO: Sua conta permanece ativa pois o saldo não esta zerado.");
         }
     }
 
     public void depositar(float deposito) {
-         if (status) {
+         if (this.status) {
             this.saldo += deposito;
+             System.out.println("Deposito realizado na conta de " + this.getDono());
          } else {
              System.out.println("ERRO: Conta não ativa.");
         }
     }
 
     public void sacar(float saque) {
-        if (status && saque <= this.saldo) {
-            this.saldo -= saque;
+        if (this.status){
+            if (saque <= this.saldo) {
+                this.saldo -= saque;
+                System.out.println("Saque realizdo com sucesso na conta de " + this.getDono());
+            } else {
+                System.out.println("ERRO: Saldo insuficiente.");
+            }
         } else {
-            System.out.println("ERRO: Saldo insuficiente");
+            System.out.println("ERRO: Conta inativa.");
         }
     }
 
     public void pagarMensal(String tipo) {
-        if (Objects.equals(tipo, "CC")) {
-            this.saldo -= 12;
-        }
-        else if (Objects.equals(tipo, "CP")) {
-            this.saldo -= 20;
+        if (this.status && saldo > 0){
+            if (Objects.equals(tipo, "CC")) {
+                this.saldo -= 12;
+            } else if (Objects.equals(tipo, "CP")) {
+                this.saldo -= 20;
+            }
         } else {
             System.out.println("ERRO: Tipo de conta invalido.");
         }
