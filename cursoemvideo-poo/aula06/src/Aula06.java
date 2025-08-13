13f9aaba12d2219bcfd6d545daf0b81c5f544424
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-08-13 16:25:54 -0300

    add: Adicionar práticas da aula 6 - Encapsulamento

---
+++ cursoemvideo-poo/aula06/src/Aula06.java
@@ -0,0 +1,9 @@
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.play();
+        c.maisVolume();
+        c.maisVolume();
+        c.abrirMenu();
+    }
+}
