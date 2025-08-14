7a721df404705e506e15cff70ab33fe686571105
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-08-14 16:21:33 -0300

    add: adicionar exercício prático poo em java

---
+++ cursoemvideo-poo/aula08/src/Pessoa.java
@@ -0,0 +1,47 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private char sexo;
+
+    public Pessoa(String nome, int idade, char sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public void fazerAniver() {
+        this.setIdade(this.getIdade() + 1);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0) {
+            System.out.println("ERRO: Idade inválida.");
+        } else {
+            this.idade = idade;
+        }
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        if (sexo == 'M' || sexo == 'F') {
+            this.sexo = sexo;
+        } else {
+            System.out.println("Sexo inválido.");
+        }
+    }
+}
