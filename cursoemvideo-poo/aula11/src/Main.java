1a227f27c8ae30baca48366e96536200d0dea017
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-08-16 15:08:00 -0300

    add: adicionar aula e exercício sobre polimorfismo

---
+++ cursoemvideo-poo/aula11/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+
+        m.setPeso(5.3f);
+        m.setCorPelo("Marrom");
+        m.alimentar();
+        m.locomover();
+        m.emitirSom();
+
+        a.locomover();
+        p.locomover();
+        r.locomover();
+    }
+}
