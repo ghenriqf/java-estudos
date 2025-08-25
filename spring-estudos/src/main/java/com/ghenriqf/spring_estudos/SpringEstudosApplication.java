d0761d7597a40ca36331d8aaf73ebf6dd49cfbcc
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-08-25 14:25:18 -0300

    add: criar projeto spring boot para estudos

---
+++ spring-estudos/src/main/java/com/ghenriqf/spring_estudos/SpringEstudosApplication.java
@@ -0,0 +1,13 @@
+package com.ghenriqf.spring_estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEstudosApplication.class, args);
+	}
+
+}
